cb8b4a4dff30194c54ecbed9b7fc73ef2ab1198b
Dominic <dominic.laube@gmail.com>
2019-08-29 17:10:31 +0200

    create gameboard object

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const gameBoard = (() => {
+let boardValues = ["x","x","o","o","x","o","o","o","o"];
+const status = () => boardValues;
+return {status};
+})();
 
+const displayController = (() => {
+
+})();
+
+const player = (name, color) => {
+  
+}
+
+console.log(gameBoard.status());
