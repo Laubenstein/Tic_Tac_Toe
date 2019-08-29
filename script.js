0b680554942b5cea6c01e2b20a5f959e85bc065e
Dominic <dominic.laube@gmail.com>
2019-08-29 20:49:37 +0200

    create diplay for gameboard, add players, ARRAY BROKE

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const gameBoard = (() => {
-let boardValues = ["x","x","o","o","x","o","o","o","o"];
-const status = () => boardValues;
-return {status};
+  let boardValues = ["x","x","o","o","x","o","o","o","o"];
+  const getValues = () => boardValues;
+  const setValue = (a, value) => boardValues[a] = value;
+  const makeBoard = () => { 
+    for (let i = 0; i < boardValues.length; i++) {
+      let boardContainer = document.querySelector('#boardContainer');
+      let field = document.createElement('div');
+      field.setAttribute('id', 'field' + i);
+      field.setAttribute('class', 'field');
+      field.addEventListener('click', setValue(i, player.getColor));
+      boardContainer.appendChild(field);
+    };
+  };
+  return {
+    getValues, 
+    setValue, 
+    makeBoard
+  };
 })();
 
 const displayController = (() => {
-
+  
 })();
 
 const player = (name, color) => {
-  
-}
-
-console.log(gameBoard.status());
+  const getName = () => name;
+  const getColor = () => color;
+  return {getName, getColor};
+};
+const player1 = player('player1', 'x');
+const player2 = player('player2', 'o');
+gameBoard.makeBoard();
+console.log(gameBoard.getValues());
