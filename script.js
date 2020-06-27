f23cb6f6f06eea1fb77512074cf60d0150ee436a
Laubenstein <dominic.laube@gmail.com>
2020-06-27 17:28:24 +0200

    add working event listener

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const gameBoard = (() => {
-  let boardValues = ["","","","","","","","",""];
-  const getValues = () => boardValues;
-  const setValue = (a, value) => boardValues[a] = value;
+  let boardArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  const getValues = (i, j) => boardArray[i][j];
+  const setValue = (a, b,  value) => boardArray[a][b] = value;
+  function change(buttonID) {
+    let elem = buttonID;
+    elem.value = player1.getColor();
+    elem.textContent = player1.getColor();
+    return elem.value;
+  }
   const makeBoard = () => {
-    for (let i = 0; i < boardValues.length; i++) {
-      let boardContainer = document.querySelector('#boardContainer');
-      let field = document.createElement('div');
-      field.setAttribute('id', 'field' + i);
-      field.setAttribute('class', 'field');
-      field.addEventListener('click', setValue(i, player.getColor));
-      boardContainer.appendChild(field);
-    };
+    let buttonID = 0;
+    for (let i = 0; i < boardArray.length; i++) {
+      let innerArray = boardArray[i];
+      for (let j = 0; j < innerArray.length; j++) {
+        let boardContainer = document.querySelector('#boardContainer');
+        let buttonElement = document.createElement('button');
+        buttonElement.setAttribute('id', buttonID.toString());
+        buttonElement.setAttribute('class', 'buttonElement');
+        buttonElement.addEventListener('click', function(event) {
+          buttonElement.value = player1.getColor();
+          buttonElement.textContent = player1.getColor();
+        });
+        boardContainer.appendChild(buttonElement);
+        buttonID++;
+    }
+    }
   };
   return {
     getValues,
@@ -30,6 +44,5 @@ const player = (name, color) => {
 };
 const player1 = player('player1', 'x');
 const player2 = player('player2', 'o');
+
 gameBoard.makeBoard();
-gameBoard.setValue(1, player1.getColor());
-console.log(gameBoard.getValues());
