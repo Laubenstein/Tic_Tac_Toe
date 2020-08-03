83dce9aef17e153302e5a2e717970b41dae02ae0
unknown <dominic.laube@gmail.com>
2020-08-03 19:49:28 +0200

    Implement player change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (() => {
+const GameBoard = (() => {
   let boardArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
   const getValues = (i, j) => boardArray[i][j];
   const setValue = (a, b,  value) => boardArray[a][b] = value;
@@ -18,8 +18,9 @@ const gameBoard = (() => {
         buttonElement.setAttribute('id', buttonID.toString());
         buttonElement.setAttribute('class', 'buttonElement');
         buttonElement.addEventListener('click', function(event) {
-          buttonElement.value = player1.getColor();
-          buttonElement.textContent = player1.getColor();
+          buttonElement.value = DisplayController.currentPlayer().getColor();
+          buttonElement.textContent = DisplayController.currentPlayer().getColor();
+          DisplayController.changePlayer();
         });
         boardContainer.appendChild(buttonElement);
         buttonID++;
@@ -33,16 +34,27 @@ const gameBoard = (() => {
   };
 })();
 
-const displayController = (() => {
-
-})();
-
-const player = (name, color) => {
+const Player = (name, color) => {
   const getName = () => name;
   const getColor = () => color;
   return {getName, getColor};
 };
-const player1 = player('player1', 'x');
-const player2 = player('player2', 'o');
+const player1 = Player('player1', 'x');
+const player2 = Player('player2', 'o');
 
-gameBoard.makeBoard();
+const DisplayController = (() => {
+  let activePlayer = player1;
+  const currentPlayer = () => activePlayer;
+  const changePlayer = () => {
+    if (activePlayer === player1) {
+      activePlayer = player2;
+    } else {
+      activePlayer = player1;
+    }
+  };
+  return {currentPlayer, changePlayer};
+})();
+
+
+
+GameBoard.makeBoard();
